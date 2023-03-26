b55ef220cf2f064fb7685a0ee0be83d8443fe727
FaresBadrCA <97144053+FaresBadrCA@users.noreply.github.com>
2023-03-25 19:36:36 -0600

    First commit.

---
+++ threePoles.cpp
@@ -0,0 +1,65 @@
+﻿/*
+* Given three poles, where the first pole has N discs stacked on top of each other in order of size, with the smallest on top
+* Transfer all the discs to the third pole, under the following rules: 
+* 1) Only one disc can be moved at a time
+* 2) A disc may only be placed on top of a larger disc
+* 
+* This program provides an iterative solution to this problem.
+*/
+
+#include "threePoles.h"
+
+using namespace std;
+
+// move discs (2,1) from p_start to p_end
+void build_21(Pole& p_start, Pole& p_temp, Pole& p_end) {
+	int disc1 = p_start.pop();
+	assert(disc1 == 1);
+	p_temp.insert(disc1);
+	
+	int disc2 = p_start.pop();
+	assert(disc2 == 2);
+	p_end.insert(disc2);
+
+	disc1 = p_temp.pop();
+	p_end.insert(disc1);
+}
+
+// move a tower (k->1) from p_start to p_end
+void build(Pole& p_start, Pole& p_temp, Pole& p_end, int k) {
+	if (k == 2) {
+		build_21(p_start, p_temp, p_end);
+		return;
+	}
+	
+	build(p_start, p_end, p_temp, k - 1); // build a (k-1 -> 1) tower in the intermediate pole
+
+	int disc_k = p_start.pop();
+	assert(disc_k == k);
+	p_end.insert(disc_k);
+
+	build(p_temp, p_start, p_end, k - 1); // build a (k-1 -> 1) tower in the end pile
+}
+
+
+int main()
+{
+	size_t STACK_SIZE = 10;
+
+	Pole p1(STACK_SIZE);
+	Pole p2(STACK_SIZE);
+	Pole p3(STACK_SIZE);
+	p1.fill();
+
+	p1.print();
+	p2.print();
+	p3.print();
+	
+	build(p1, p2, p3, STACK_SIZE);
+
+	p1.print();
+	p2.print();
+	p3.print();
+
+	return 0;
+}
---
+++ threePoles.h
@@ -0,0 +1,53 @@
+﻿// threePoles.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#include "assert.h"
+#include <iostream>
+#include <vector>
+
+class Pole {
+public:
+	int* discs;
+	size_t SIZE;
+	size_t MAX_SIZE;
+
+	Pole(size_t max_size) {
+		MAX_SIZE = max_size;
+		SIZE = 0;
+		discs = new int[MAX_SIZE];
+	}
+
+	~Pole() {
+		delete[] discs;
+	}
+
+	void fill() {
+		for (int i = 0; i < MAX_SIZE; ++i) {
+			discs[i] = MAX_SIZE - i;
+		}
+		SIZE = MAX_SIZE;
+	}
+
+	void insert(int k) {
+		if (SIZE > 0) {
+			assert(k < discs[SIZE - 1]); // disc being inserted is smaller than last disc
+		}
+		discs[SIZE++] = k;
+	}
+
+	int pop() {
+		assert(SIZE > 0);
+		return discs[SIZE-- - 1]; // return last disc, and decrement size
+	}
+
+
+	void print() {
+		std::cout << "POLE: ";
+		for (int i = 0; i < SIZE; ++i) {
+			std::cout << discs[i] << ", ";
+		}
+		std::cout << "\n";
+	}
+
+};
